52d035a39a79ed7ae2fa0862d89d52e32495c412
Maro <Maro.Radovic@smithsnews.co.uk>
2018-05-18 15:53:55 +0100

    feat(Shared Module):

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+  ]
+})
+export class SharedModule {
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
+  }
+}
