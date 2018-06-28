42d6629c7c4b20d413c2861a992963a890d5476b
Maro <Maro.Radovic@smithsnews.co.uk>
2018-06-28 08:47:51 +0100

    feat(Contact Page): Contact Form

--- src/app/+contact-page/contact-page.component.ts
+++ src/app/+contact-page/contact-page.component.ts
@@ -1,5 +1,6 @@
 // Core
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 // App specific
 
@@ -8,8 +9,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'contact-page.component.html'
 })
 export class ContactPageComponent implements OnInit {
-  constructor() {}
+
+  contactForm: FormGroup;
+
+  nameError = '';
+  emailError = '';
+  messageError = '';
+
+  private nameValidationMessages = {
+    required: 'Please fill in your name'
+  };
+
+  private emailValidationMessages = {
+    required: 'Please insert your email address',
+    pattern: 'Email format seems to be invalid'
+  };
+
+  private bodyValidationMessages = {
+    required: 'Please insert your message'
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  private setName(c: AbstractControl): void {
+    this.nameError = '';
+    if ((c.touched || c.dirty) && c.errors) {
+
+    }
+  }
 
   ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required]]
+    });
+
+    const nameControl = this.contactForm.get('name');
+    nameControl.valueChanges.debounceTime(800).subscribe();
   }
 }
--- src/app/+contact-page/contact-page.module.ts
+++ src/app/+contact-page/contact-page.module.ts
@@ -6,9 +6,16 @@ import { ContactPageComponent } from './contact-page.component';
 import { ContactPageService } from './contact-page.service';
 import { SharedModule } from '../shared/shared.module';
 import { ContactPageRoutes } from './contact-page.routes';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../shared/materialModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
     SharedModule,
     ContactPageRoutes
   ],
