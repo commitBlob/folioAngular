af590e66d91ce90ab46bdd238cfc5ec38cf383da
Maro <Maro.Radovic@smithsnews.co.uk>
2018-08-17 14:31:06 +0100

    feat(Projects Page): Return "button"

--- src/app/+portfolio-page/project-details/project-details.module.ts
+++ src/app/+portfolio-page/project-details/project-details.module.ts
@@ -1,13 +1,17 @@
 // Core
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 // App specific
 import { ProjectDetailsComponent } from './project-details.component';
 import { ProjectDetailsService } from './project-details.service';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   declarations: [
     ProjectDetailsComponent
   ],
