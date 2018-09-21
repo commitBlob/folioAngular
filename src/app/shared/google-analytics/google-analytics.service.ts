e25d916a8e53be2c142940319d4ffdc168a0e495
Maro <Maro.Radovic@smithsnews.co.uk>
2018-09-21 11:40:49 +0100

    feat(Google Analytics): Hook up analytics

---
+++ src/app/shared/google-analytics/google-analytics.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare var ga: Function;
+
+@Injectable()
+export class GoogleAnalyticsService {
+
+  constructor(private router: Router) {
+    router.events.subscribe( event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.url);
+        ga('send', 'pageview');
+      }
+    });
+  }
+}
