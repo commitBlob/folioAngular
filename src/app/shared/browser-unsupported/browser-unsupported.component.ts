e9dcabdc2d7f1c7749c6f9ecbae213b53ecf7910
commitBlob <maro.radovic12@gmail.com>
2020-07-06 11:05:25 +0100

    feat(Browser Support): Browser unsupported page and browser detect service

---
+++ src/app/shared/browser-unsupported/browser-unsupported.component.ts
@@ -0,0 +1,9 @@
+// Core
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browser-unsupported',
+  templateUrl: 'browser-unsupported.component.html'
+})
+export class BrowserUnsupportedComponent {
+}
